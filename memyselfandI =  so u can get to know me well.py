698801881f94f0f947f63b6bf14c19c0d7ff94f8
vajraexe <vajrajani2011@gmail.com>
2024-09-25 19:06:39 +0100

    Add files via upload

---
+++ memyselfandI =  so u can get to know me well.py
@@ -0,0 +1,10 @@
+memyselfandI = {
+        "name" : "Vajra.exe>:)",
+        "age" : "12",
+        "gender" : "girl but not a girly girl, a tomboy",
+        "status" : "currently unemployed, a student hehe",
+        "hobbies,plural dat right" : "piano, video games, sleeping yay",
+        "stuff I hate" : "books, school, house chores, being smushed in my school bus", 
+    }
+print(memyselfandI)
+print(memyselfandI["stuff I hate"])
